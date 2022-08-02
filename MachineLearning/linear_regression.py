7d4cefb40f9b520b37b0ba675dd96a59bf0fad2f
Ze Yu <yuze@iphy.ac.cn>
2022-08-02 21:45:24 +0800

    Linear Regression.

--- MachineLearning/linear_regression.py
+++ MachineLearning/linear_regression.py
@@ -2,6 +2,7 @@ import random
 
 import numpy as np
 import pandas as pd
+from sklearn.model_selection import train_test_split
 
 
 class LinearRegession:
@@ -11,6 +12,7 @@ class LinearRegession:
         else:
             self.X = X
             self.Y = Y
+        self.seed = seed
         self.n_samples, self.n_features = self.X.shape
         self.init_w(self.n_features)
         self.data_normalization()
@@ -43,43 +45,77 @@ class LinearRegession:
         self.X = np.concatenate((np.ones((self.n_samples, 1)), self.X), axis=1)
 
     def train(self, batch_size=1, epoches=400, lr=0.01, method="SGD"):
-        self.loss_history = []
-        for i in range(1, epoches + 1):
-            y_pred = np.dot(self.X, self.w)
-            loss = self.get_loss(y_pred, self.Y)
-            self.loss_history.append(loss)
-            if not i % 10:
-                print(f"---- epoch {i} / {epoches} ----")
-                print(f"Loss: {round(loss, 3)}")
+        # split train valid set
+        train, valid = train_test_split(
+            np.concatenate((self.X, self.Y), axis=1),
+            train_size=0.7,
+            random_state=self.seed,
+            shuffle=True
+        )
+        train_X, train_Y = train[:, :-1], train[:, -1].reshape((-1, 1))
+        valid_X, valid_Y = valid[:, :-1], valid[:, -1].reshape((-1, 1))
 
+        n_samples = train_X.shape[0]
+        self.loss_history = []
+        for i in range(epoches):
+            y_pred_train = np.dot(train_X, self.w)
+            y_pred_valid = np.dot(valid_X, self.w)
+            train_loss = self.get_loss(y_pred_train, train_Y)
+            valid_loss = self.get_loss(y_pred_valid, valid_Y)
+            self.loss_history.append([train_loss, valid_loss])
             if method == "SGD":
-                for j in range(0, self.n_samples, batch_size):
-                    diff_rad = y_pred[j: j + batch_size] - self.Y[j: j + batch_size]
-                    dw = (self.X[j: j + batch_size].T.dot(diff_rad))/ self.n_samples
+                for j in range(0, n_samples, batch_size):
+                    diff_rad = y_pred_train[j: j + batch_size] - train_Y[j: j + batch_size]
+                    dw = (train_X[j: j + batch_size].T.dot(diff_rad)) / n_samples
                     self.w = self.w - lr * dw
 
                 pre = self.n_samples // batch_size * batch_size
                 if pre != self.n_samples:
-                    diff_rad = y_pred[pre:] - self.Y[pre:]
-                    dw = (self.X[pre:].T.dot(diff_rad)) / self.n_samples
+                    diff_rad = y_pred_train[pre:] - train_Y[pre:]
+                    dw = (train_X[pre:].T.dot(diff_rad)) / n_samples
                     self.w = self.w - lr * dw
 
             elif method == "GD":
-                diff = y_pred - self.Y
-                dw = self.X.T.dot(diff) / self.n_samples
+                diff = y_pred_train - train_Y
+                dw = train_X.T.dot(diff) / n_samples
                 self.w = self.w - lr * dw
+
+            if not i % 10:
+                print(f"---- epoch {i} / {epoches} ----")
+                print(f"Train Loss: {round(train_loss, 3)}  Loss valid: {round(valid_loss, 3)}")
+
+    def fit_by_normal_equation(self):
+        self.w = np.linalg.inv(np.dot(self.X.T, self.X)).dot(self.X.T).dot(self.Y)
 
     def predict(self, x):
         return (self.w[0] + np.dot(x, self.w[1:]))[0]
 
     def plot_loss_history(self):
         from matplotlib import pyplot as plt
-        plt.plot(self.loss_history)
+        plt.plot([i[0] for i in self.loss_history], label="train loss", color="b")
+        plt.plot([i[1] for i in self.loss_history], label="valid loss", color="r")
         plt.xlabel("Epoches")
         plt.ylabel("Loss")
         plt.show()
 
 
-lr = LinearRegession()
-lr.train(method="SGD", batch_size=20)
-print(lr.predict(np.random.random(13)))
+if __name__ == '__main__':
+    lr = LinearRegession()
+    # two kinds of methods:
+    # method one: noraml equation
+    normal_equation = True
+    if normal_equation:
+        lr.fit_by_normal_equation()
+    else:
+        # method two: train
+        lr.train(method="SGD", batch_size=20, epoches=800)
+
+    # random predict
+    choice = np.random.randint(0, lr.X.shape[0], 5)
+    for i in choice:
+        p = round(lr.predict(lr.X[i][1:]), 3)
+        print(f"Fact: {lr.Y[i][0]}  Predict: {p}")
+
+    # plot loss
+    if not normal_equation:
+        lr.plot_loss_history()
